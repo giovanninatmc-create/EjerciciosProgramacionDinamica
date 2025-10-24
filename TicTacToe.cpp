787d1c47396faaba23161cde8a7a97132bd3e361
giovanninatmc-create <giovanni.nat.mc@gmail.com>
2025-10-24 13:40:10 -0600

    Version Final
    Se añadio el modo vs CPU modificando la funcion jugar( ) , ademas de modificar la funcion elegirNombres, y los contadores de victorias, tambien se agregan algunos comentarios extras en partes importantes del codigo.

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,17 +4,21 @@
 #include <ctime>
 #ifdef _WIN32
 #include <conio.h>
+#include <windows.h>
+#else
+#include <unistd.h>
 #endif
 using namespace std;
 
 // =============================================================
 // PROTOTIPOS
+// Nos sirve para inicializar las funciones en cualquier momento
 // =============================================================
 void mostrarInicio();
-void pedirNombres(string &jugador1, string &jugador2);
+void pedirNombres(string &jugador1, string &jugador2, bool vsCPU);
 int elegirTurnoAleatorio(string jugador1, string jugador2);
 void limpiarPantalla();
-int jugar(string jugador1, string jugador2, int turno);
+int jugar(string jugador1, string jugador2, int turno, bool vsCPU);
 void mostrarTablero(char tablero[3][3]);
 bool hayGanador(char tablero[3][3]);
 bool tableroLleno(char tablero[3][3]);
@@ -29,6 +33,8 @@ int main()
     int victoriasJ1 = 0, victoriasJ2 = 0;
     char opcion;
     bool volverMenu = true;
+    bool vsCPU = false;
+    int modo;
 
     do
     {
@@ -40,23 +46,41 @@ int main()
 #endif
 
         limpiarPantalla();
-        pedirNombres(jugador1, jugador2);
 
-        victoriasJ1 = 0;
+        // SELECCION DE MODO DE JUEGO
+        // Pide al usuario seleccionar el modo de juego y lo lee la variable modo
+        cout << "==============================================================" << endl;
+        cout << "Elige modo de juego:" << endl;
+        cout << "1. Jugador vs Jugador" << endl;
+        cout << "2. Jugador vs CPU" << endl;
+        cout << "==============================================================" << endl;
+        cout << "Opcion: ";
+        cin >> modo;
+        cin.ignore();
+
+        if (modo == 2) // Valida el modo de juego con una variable bool
+            vsCPU = true;
+        else
+            vsCPU = false;
+
+        pedirNombres(jugador1, jugador2, vsCPU); // Llama la funcion pedir nombres
+
+        victoriasJ1 = 0; // Inicializamos las variables de victorias en 0
         victoriasJ2 = 0;
 
-        char jugarOtraVez;
+        char jugarOtraVez; // Declaramos una variable para la funcion de jugarOtraVez
         do
         {
             limpiarPantalla();
-            int turno = elegirTurnoAleatorio(jugador1, jugador2);
-            int resultado = jugar(jugador1, jugador2, turno);
+            int turno = elegirTurnoAleatorio(jugador1, jugador2);    // Llamamos a la funcion para que la compu decida quien inicia primero
+            int resultado = jugar(jugador1, jugador2, turno, vsCPU); // Inicia la partida llamando a la funcion jugar
 
-            if (resultado == 1)
+            if (resultado == 1) // La variable resultado incrementa en 1 las victorias dependiendo que jugardor gana
                 victoriasJ1++;
             else if (resultado == 2)
                 victoriasJ2++;
 
+            // ESta parte del codigo muestra el marcador actual entre los jugadores
             cout << endl;
             cout << "=================================" << endl;
             cout << " MARCADOR ACTUAL" << endl;
@@ -65,6 +89,7 @@ int main()
             cout << jugador2 << ": " << victoriasJ2 << " victoria(s)" << endl;
             cout << "=================================" << endl;
 
+            // Preguntamos al usuario si quiere jugar otra vez
             cout << "\nQuieres jugar otra vez? (S/N): ";
             cin >> jugarOtraVez;
             cin.ignore();
@@ -72,17 +97,17 @@ int main()
         } while (toupper(jugarOtraVez) == 'S');
 
         limpiarPantalla();
-        mostrarResultadosFinales(jugador1, jugador2, victoriasJ1, victoriasJ2);
+        mostrarResultadosFinales(jugador1, jugador2, victoriasJ1, victoriasJ2); // Llamamos a una funcion para mostrar el resultado final del juego
 
-        cout << "\nQuieres volver al menu principal? (S/N): ";
+        cout << "\nQuieres volver al menu principal? (S/N): "; // Preguntamos si queremos volver al menu principal o cerrar el juego
         cin >> opcion;
         cin.ignore();
 
-        if (toupper(opcion) == 'N')
+        if (toupper(opcion) == 'N') // no diferenciar entre mayuscula y minuscula
         {
             volverMenu = false;
             limpiarPantalla();
-            cout << "==============================================================" << endl;
+            cout << "==============================================================" << endl; // Si elegimos cerrar el juego sale un mensaje de agradecimiento
             cout << "   Gracias por jugar Tic Tac Toe  - Nos vemos la proxima      " << endl;
             cout << "==============================================================" << endl;
             cout << "Presiona una tecla para salir...";
@@ -101,7 +126,7 @@ int main()
 // =============================================================
 // DEFINICIONES DE FUNCIONES
 // =============================================================
-void mostrarInicio()
+void mostrarInicio() // Muestra las reglas del juego
 {
     cout << "==============================================================" << endl;
     cout << "|                 ****** BIENVENIDO A ******                 |" << endl;
@@ -143,22 +168,33 @@ void mostrarInicio()
     cout << "==============================================================" << endl;
     cout << "Presiona cualquier tecla para continuar..." << endl;
 }
-void pedirNombres(string &jugador1, string &jugador2)
+
+void pedirNombres(string &jugador1, string &jugador2, bool vsCPU) // Pide al usuario los nombres de los jugadores
 {
     cout << "==============================================================" << endl;
     cout << "NOMBRE DE LOS JUGADORES" << endl;
     cout << "==============================================================" << endl;
-    cout << "Ingresa el nombre del jugador 1: ";
+    cout << "Ingresa el nombre del jugador 1: "; // Pide por default el numero del jugador 1
     getline(cin, jugador1);
-    cout << "Ingresa el nombre del jugador 2: ";
-    getline(cin, jugador2);
+
+    if (vsCPU)
+    {
+        jugador2 = "CPU";
+        cout << "Modo CPU activado. Te enfrentaras a la computadora." << endl; // Si elegimos CPU aparece ese mensaje
+    }
+    else
+    {
+        cout << "Ingresa el nombre del jugador 2: "; // Si elegimos contra otro jugador nos pide su nombre
+        getline(cin, jugador2);
+    }
+
     cout << "==============================================================" << endl;
 }
 
 int elegirTurnoAleatorio(string jugador1, string jugador2)
 {
     srand(time(0));
-    int turno = rand() % 2; // 0 o 1
+    int turno = rand() % 2; // 0 o 1, se generan estos dos valores y se elige aleatoriamente quien empieza el juego
     cout << endl;
     cout << "==============================================================" << endl;
     cout << "Que los Dioses del Tic Tac Toe decidan quien empieza..." << endl;
@@ -173,7 +209,7 @@ int elegirTurnoAleatorio(string jugador1, string jugador2)
     return turno;
 }
 
-void limpiarPantalla()
+void limpiarPantalla() // Funcion para limpiar pantalla, ya sea Linux o Windows
 {
 #ifdef _WIN32
     system("cls");
@@ -184,33 +220,49 @@ void limpiarPantalla()
 
 // =============================================================
 // FUNCION PRINCIPAL DEL JUEGO
-// Devuelve: 1 si gana jugador1, 2 si gana jugador2, 0 si hay empate
 // =============================================================
-int jugar(string jugador1, string jugador2, int turno)
+int jugar(string jugador1, string jugador2, int turno, bool vsCPU)
 {
-    char tablero[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+    char tablero[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}}; // arreglo para las casillas del tablero
     char marca;
     int casilla;
-    bool ganador = false;
+    bool ganador = false; // inicializamos la variable de ganador como false hasta que se cumpla y sea true
 
     do
     {
         limpiarPantalla();
-        mostrarTablero(tablero);
+        mostrarTablero(tablero); // Mostramos el tablero vacio
 
         // Determinar jugador actual
         if (turno % 2 == 0)
         {
             cout << jugador1 << " (X), elige una casilla: ";
             marca = 'X';
+            cin >> casilla;
         }
         else
         {
-            cout << jugador2 << " (O), elige una casilla: ";
             marca = 'O';
+            if (vsCPU)
+            {
+                cout << "Turno de la CPU (O)..." << endl;
+#ifdef _WIN32
+                Sleep(700);
+#else
+                usleep(700000);
+#endif
+                do
+                {
+                    casilla = rand() % 9 + 1;
+                } while (tablero[(casilla - 1) / 3][(casilla - 1) % 3] == 'X' ||
+                         tablero[(casilla - 1) / 3][(casilla - 1) % 3] == 'O');
+            }
+            else
+            {
+                cout << jugador2 << " (O), elige una casilla: ";
+                cin >> casilla;
+            }
         }
-
-        cin >> casilla;
 
         // Validar casilla
         if (casilla < 1 || casilla > 9)
@@ -276,7 +328,7 @@ int jugar(string jugador1, string jugador2, int turno)
         }
 
         // Cambiar turno
-        turno++;
+        turno++; // incrementamos el uno siempre en uno, cuando sea par es jugador1, cuando se impar jugador 2
 
     } while (!tableroLleno(tablero));
 
